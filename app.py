d89045d05c0467f78124f09d43a80fe759d599ae
Ahuvim Maor <Ahuvim5@gmail.com>
2019-04-15 17:53:27 +0300

    added a DialoFlow library

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from joke import joke
 import requests
 from cleverwrap import CleverWrap
-
-cw = CleverWrap("API_KEY")
+from dialog import fetch_replay
 
 app = Flask(__name__)
 
@@ -17,23 +16,23 @@ def hello():
 def sms_reply():
     resp = MessagingResponse()
     msg = request.form.get("Body")
+    phone_number = request.form.get("From")
+    replay = fetch_replay(msg, phone_number)
+    resp.message(replay)
 
-    bot_resp = cw.say(msg)
-    resp.message(bot_resp)
-    url = "https://icanhazdadjoke.com/search"
-
-    res = requests.get(
-        url,
-        # we want json respons
-        headers={"Accept": "application/json"},
-        params={"term": msg}
-    ).json()
-
-    if(msg == 'היי' or msg == "מה קורה?"):
-        resp.message(f"היי מה קורה? תשלח לי נושא בדיחה באנגלית ואשלח לך בדיחה!!!")
-    else:
-        resp.message(joke(msg, res))
     return str(resp)
+
+    # url = "https://icanhazdadjoke.com/search"
+    # res = requests.get(
+    #     url,
+    #     # we want json respons
+    #     headers={"Accept": "application/json"},
+    #     params={"term": msg}
+    # ).json()
+    # if(msg == 'היי' or msg == "מה קורה?"):
+    #     resp.message(f"היי מה קורה? תשלח לי נושא בדיחה באנגלית ואשלח לך בדיחה!!!")
+    # else:
+    #     resp.message(joke(msg, res))
 
 if __name__ == "__main__":
     app.run(debug=True)
