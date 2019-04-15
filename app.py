199456e7f801a0cae3701676057c4225606b6b47
Ahuvim Maor <Ahuvim5@gmail.com>
2019-04-15 11:45:44 +0300

    added jokes

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from joke import joke
 import requests
-from cleverwrap import CleverWrap
+# from cleverwrap import CleverWrap
 
 app = Flask(__name__)
 
-cleverbot_API = CleverWrap("INSERT YOUR API KEY")
+# cleverbot_API = CleverWrap("INSERT YOUR API KEY")
 
 @app.route("/")
 def hello():
@@ -16,24 +16,24 @@ def hello():
 def sms_reply():
     resp = MessagingResponse()
     msg = request.form.get("Body")
-    cleverbot_response = cleverbot_API.say(msg)
+    # cleverbot_response = cleverbot_API.say(msg)
 
-    # url = "https://icanhazdadjoke.com/search"
+    url = "https://icanhazdadjoke.com/search"
 
-    # res = requests.get(
-    #     url,
-    #     # we want json respons
-    #     headers={"Accept": "application/json"},
-    #     params={"term": msg}
-    # ).json()
+    res = requests.get(
+        url,
+        # we want json respons
+        headers={"Accept": "application/json"},
+        params={"term": msg}
+    ).json()
 
-    # if(msg == 'היי' or msg == "מה קורה?"):
-    #     resp.message(f"היי מה קורה? תשלח לי נושא בדיחה באנגלית ואשלח לך בדיחה!!!")
-    # else:
-    #     resp.message(joke(msg, res))
-    # return str(resp)
-    resp.message(cleverbot_response)
+    if(msg == 'היי' or msg == "מה קורה?"):
+        resp.message(f"היי מה קורה? תשלח לי נושא בדיחה באנגלית ואשלח לך בדיחה!!!")
+    else:
+        resp.message(joke(msg, res))
     return str(resp)
+    # resp.message(cleverbot_response)
+    # return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
